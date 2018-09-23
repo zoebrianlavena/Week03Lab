c028dcb8c011e09d033fe0a1473cd468a276ff74
743953 <743953@elf7it218937.ACDM.DS.SAIT.CA>
2018-09-23 17:21:18 -0600

    Week 03 Lab Created jsp's, servlet, and mapped the url.

---
+++ src/java/models/Note.java
@@ -0,0 +1,37 @@
+
+package models;
+
+/**
+ *
+ * @author 743953
+ */
+public class Note {
+    String title;
+    String content;
+    
+    public Note(){
+        
+    }
+    
+    public Note(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
---
+++ web/WEB-INF/viewnote.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : viewnote
+    Created on : Sep 23, 2018, 5:13:01 PM
+    Author     : 743953
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
